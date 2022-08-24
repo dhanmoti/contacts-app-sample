25a7afe49d11b3bfe6f26c10a5c113479540893a
devidhan <dhanmoti@icloud.com>
2022-08-24 22:10:39 +0800

    add draft contact list view

---
+++ ContactsApp/ContactsApp/ContactCell.swift
@@ -0,0 +1,23 @@
+//
+//  ContactCell.swift
+//  ContactsApp
+//
+//  Created by Dhan Moti on 24/8/22.
+//
+
+import UIKit
+
+class ContactCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
